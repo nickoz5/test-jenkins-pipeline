fdf599ce03fe4f6674f239a359f2a004de2e29b1
Nick Hynes <nickoz5@users.noreply.github.com>
2020-12-10 16:22:53 +0000

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,7 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            test();
+            etwefsdfstest();
             //another test()
         }
     }
